c13ccbe23536b12ce8b64501df94dc2e0923af5d
babatonha <tonhaview@gmail.com>
2022-04-08 19:55:34 +0200

    initial create

---
+++ eCommerceAPI/eCommerceAPI/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using Core.Entities;
+using eCommerceAPI.Data;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+
+            return Ok(products);
+        }
+
+    }
+}
