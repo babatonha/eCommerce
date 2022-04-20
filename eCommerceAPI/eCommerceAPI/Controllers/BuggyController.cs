646999787edc689c4b194ede8a1991e37d2f3bb1
babatonha <tonhaview@gmail.com>
2022-04-20 22:37:37 +0200

    Section 11

--- eCommerceAPI/eCommerceAPI/Controllers/BuggyController.cs
+++ eCommerceAPI/eCommerceAPI/Controllers/BuggyController.cs
@@ -17,11 +17,9 @@ namespace eCommerceAPI.Controllers
         {
             var thing = _context.Products.Find(42);
 
-            if (thing == null)
-            {
-                return NotFound(new ApiResponse(404));
-            }
-            return Ok(thing);
+            if (thing == null) return NotFound(new ApiResponse(404));
+
+            return Ok();
         }
 
         [HttpGet("servererror")]
@@ -29,8 +27,20 @@ namespace eCommerceAPI.Controllers
         {
             var thing = _context.Products.Find(42);
 
-            var thingToRetrun = thing.ToString();
+            var thingToReturn = thing.ToString();
 
+            return Ok();
+        }
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetNotFoundRequest(int id)
+        {
             return Ok();
         }
 
--- eCommerceUI/src/app/app.module.ts
+++ eCommerceUI/src/app/app.module.ts
@@ -4,8 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import { CoreModule } from './core/core.module';
     HttpClientModule,
     CoreModule,
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
